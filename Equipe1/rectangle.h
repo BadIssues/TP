c7797a6289b26a07f99e92de37fef2e972a025d7
BadIssues <dammat2004@Gmail.com>
2023-05-03 16:16:43 +0200

    Ajoutez des fichiers rectangle au projet.

---
+++ Equipe1/rectangle.h
@@ -0,0 +1,12 @@
+#pragma one
+#include <iostream>
+using namespace std
+
+class Rectangle {
+private:
+	int edge_width;
+	std::string edge_color;
+	std::string fill_color;
+public:
+	Rectangle(int edge_width, stdedge_color, fill_color);
+};
