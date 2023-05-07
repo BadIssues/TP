840bb86e6d35e61be9b0261b30313d09d2dff0cb
BadIssues <131671589+BadIssues@users.noreply.github.com>
2023-05-07 14:32:30 +0200

    suppression Formes.cpp car doublon

--- TP/Formes.cpp
+++
@@ -1,6 +0,0 @@
-#include "Formes.h"
-
-// Implémentation vide pour la fonction dessiner(), qui doit être surchargée dans les classes filles
-void Formes::dessiner(cv::Mat& image) const {
-    // Ne rien faire
-}
