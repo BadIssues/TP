70db4b5cf4426836ca9b2f89c2894d6103d344b5
Luan.P <parizot.luan.2b@gmail.com>
2023-05-17 11:10:10 +0200

    test pour la création d'une ligne ( fonction ligne pas encore créé )

--- UnitéTest/UnitéTest.cpp
+++ UnitéTest/UnitéTest.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../TP/main.cpp"  
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +9,30 @@ namespace UnitéTest
 	TEST_CLASS(UnitéTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestDessinerLigne_Longueur5)
 		{
+			std::stringstream output;
+			std::streambuf* oldCoutBuffer = std::cout.rdbuf();
+			std::cout.rdbuf(output.rdbuf());
+
+			dessinerLigne(5);
+
+			std::cout.rdbuf(oldCoutBuffer);
+
+			Assert::AreEqual(output.str(), std::string("-----\n"));
+		}
+
+		TEST_METHOD(TestDessinerLigne_Longueur10)
+		{
+			std::stringstream output;
+			std::streambuf* oldCoutBuffer = std::cout.rdbuf();
+			std::cout.rdbuf(output.rdbuf());
+
+			dessinerLigne(10);
+
+			std::cout.rdbuf(oldCoutBuffer);
+
+			Assert::AreEqual(output.str(), std::string("----------\n"));
 		}
 	};
 }
