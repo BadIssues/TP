60dd365735cfaeafabf7a452eee22053bf4b7f0d
Luan.P <parizot.luan.2b@gmail.com>
2023-05-17 11:00:05 +0200

    ajout du test unitaire

---
+++ UnitéTest/UnitéTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitéTest
+{
+	TEST_CLASS(UnitéTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
