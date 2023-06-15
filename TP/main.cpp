61f6700125df94411e07ad7d50d5fc2f4f545bee
BadIssues <dammat2004@Gmail.com>
2023-06-15 22:10:48 +0200

    Transformation du main.cpp : *Lire les commandes depuis le fichier cmd.txt *Utlisation d'un unique vector layers pour stocker les formes avec pointeur intelligent

--- TP/main.cpp
+++ TP/main.cpp
@@ -10,95 +10,43 @@
 
 int main()
 {
-    //Déclarer une image OpenCV
-    cv::Mat image(cv::Size(400, 400), CV_8UC3, cv::Scalar(255, 255, 255)); //Couleur sous format BGR et non RGB
-    
-    /*//Déclarer un vector permettant le stockage des formes
-    std::vector<Forme*> canva;
+    // Déclarer une image OpenCV
+    cv::Mat image(cv::Size(400, 400), CV_8UC3, cv::Scalar(255, 255, 255)); // Couleur sous format BGR et non RGB
 
-    //Créer deux instances de Cercle
-    Cercle c1(200, 200, 50, 4, cv::Scalar(0, 255, 0), cv::Scalar(255, 0, 0));
-    Cercle c2(400, 200, 50, 4, cv::Scalar(255, 0, 0), cv::Scalar(0, 0, 255));
-    //Créer deux instances de Rectangle
-    Rectangle r1(150, 150, 100, 50, 2, cv::Scalar(0, 0, 255), cv::Scalar(255, 0, 0));
-    Rectangle r2(250, 250, 50, 100, 4, cv::Scalar(255, 0, 0), cv::Scalar(255, 255, 0));
-    //Créer une instance de Ligne
-    Ligne l1(100, 100, 50, 2, cv::Scalar(0, 0, 255), cv::Scalar(0, 0, 0));
-    //Créer une instance de Triangle
-    Triangle t1(100, 300, 40, 2, cv::Scalar(255, 100, 200), cv::Scalar(0, 0, 0));
+    // Déclarer un vecteur rassemblant les pointeurs de toutes les formes
+    std::vector<std::unique_ptr<Forme>> layers;
 
-    //Ajout des formes dans le vector
-    canva.push_back(&c1);
-    canva.push_back(&c2);
-    canva.push_back(&r1);
-    canva.push_back(&r2);
-    canva.push_back(&l1);
-    canva.push_back(&t1);
-
-    //Dessin du contenu du vector
-    for (int i = 0; i < canva.size(); ++i)
-    {
-        canva.at(i)->dessiner(image);
-    }*/
-
-
-
-    //Déclarer des vector permettant le stockage des formes
-    std::vector<Cercle> circleLayers;
-    std::vector<Rectangle> rectangleLayers;
-    std::vector<Ligne> lineLayers;
-    std::vector<Triangle> triangleLayers;
-    //Déclarer un vector rassemblant l'adresse de toutes les formes
-    std::vector<Forme*> layers;
-
-    //Lire le fichier texte
+    // Lire le fichier texte
     std::vector<std::vector<std::string>> cmd = readFile();
 
     for (int i = 0; i < cmd.size(); i++)
     {
         if (cmd[i][0] == "cercle")
         {
-            Cercle c(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
-            circleLayers.push_back(c);
+            std::unique_ptr<Cercle> c = std::make_unique<Cercle>(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
+            layers.push_back(std::move(c));
         }
         else if (cmd[i][0] == "rectangle")
         {
-            Rectangle r(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), std::stoi(cmd[i][6]), cv::Scalar(std::stoi(cmd[i][7]), std::stoi(cmd[i][8]), std::stoi(cmd[i][9])), cv::Scalar(std::stoi(cmd[i][10]), std::stoi(cmd[i][11]), std::stoi(cmd[i][12])));
-            rectangleLayers.push_back(r);
+            std::unique_ptr<Rectangle> r = std::make_unique<Rectangle>(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), std::stoi(cmd[i][6]), cv::Scalar(std::stoi(cmd[i][7]), std::stoi(cmd[i][8]), std::stoi(cmd[i][9])), cv::Scalar(std::stoi(cmd[i][10]), std::stoi(cmd[i][11]), std::stoi(cmd[i][12])));
+            layers.push_back(std::move(r));
         }
-        if (cmd[i][0] == "ligne")
+        else if (cmd[i][0] == "ligne")
         {
-            Ligne l(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
-            lineLayers.push_back(l);
+            std::unique_ptr<Ligne> l = std::make_unique<Ligne>(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
+            layers.push_back(std::move(l));
         }
-        if (cmd[i][0] == "triangle")
+        else if (cmd[i][0] == "triangle")
         {
-            Triangle t(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
-            triangleLayers.push_back(t);
+            std::unique_ptr<Triangle> t = std::make_unique<Triangle>(std::stoi(cmd[i][2]), std::stoi(cmd[i][3]), std::stoi(cmd[i][4]), std::stoi(cmd[i][5]), cv::Scalar(std::stoi(cmd[i][6]), std::stoi(cmd[i][7]), std::stoi(cmd[i][8])), cv::Scalar(std::stoi(cmd[i][9]), std::stoi(cmd[i][10]), std::stoi(cmd[i][11])));
+            layers.push_back(std::move(t));
         }
-    }
-
-    for (int i = 0; i < circleLayers.size(); i++)
-    {
-        layers.push_back(&circleLayers[i]);
-    }
-    for (int i = 0; i < rectangleLayers.size(); i++)
-    {
-        layers.push_back(&rectangleLayers[i]);
-    }
-    for (int i = 0; i < lineLayers.size(); i++)
-    {
-        layers.push_back(&lineLayers[i]);
-    }
-    for (int i = 0; i < triangleLayers.size(); i++)
-    {
-        layers.push_back(&triangleLayers[i]);
     }
 
     //Dessin du contenu du vector
     for (int i = 0; i < layers.size(); ++i)
     {
-        layers.at(i)->dessiner(image);
+        layers[i] ->dessiner(image);
     }
 
 
